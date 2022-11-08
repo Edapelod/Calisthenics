eebc2f04fdd5f15145224fe87da1b5054ae3b8d5
ItsMrDav <davutt.simsek@gmail.com>
2022-11-08 15:05:35 +0100

    base style add

--- routes/auth.js
+++ routes/auth.js
@@ -1,65 +1,61 @@
 const router = require("express").Router();
 const { Router } = require("express");
-const User = require('../models/User.model.js');
-const bcrypt = require('bcryptjs')
+const User = require("../models/User.model.js");
+const bcrypt = require("bcryptjs");
 
 /* GET signup page */
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup");
-  });
-
-
+  res.render("auth/signup");
+});
 
 router.post("/signup", async (req, res) => {
-    try {
-        const salt = bcrypt.genSaltSync(11)
-        const passwordHash = bcrypt.hashSync(req.body.password, salt)
-        await User.create({
-            username : req.body.username,
-            password : passwordHash
-        })
-        res.redirect("login");
-    } catch (error) {
-        console.log(error.message)
-        res.render('auth/signup', { errorMessage: error.message})
-      }
-})
+  try {
+    const salt = bcrypt.genSaltSync(11);
+    const passwordHash = bcrypt.hashSync(req.body.password, salt);
+    await User.create({
+      username: req.body.username,
+      password: passwordHash,
+    });
+    res.redirect("login");
+  } catch (error) {
+    console.log(error.message);
+    res.render("auth/signup", { errorMessage: error.message });
+  }
+});
 
-      /* GET login page */
+/* GET login page */
 router.get("/login", (req, res, next) => {
-    res.render("auth/login");
-  });
+  res.render("auth/login");
+});
 
 router.post("/login", async (req, res) => {
-    console.log('SESSION =====> ', req.session);
-    try {
-        const {username, password} = req.body
-        const currentUser = await User.findOne({username})
-        if (!currentUser) {
-            res.render('auth/login', {errorMessage: "No user with this username"})
-        } else {
-            if (bcrypt.compareSync(password, currentUser.password)) {
-                req.session.user = currentUser
-                res.redirect('/profile')
-            } else {
-                res.render('auth/login', {errorMessage: 'Incorrect password'})
-            }
-        }
-         } catch (error) {
-            console.log(error.message)
-            res.render('auth/signup', { errorMessage: error.message})
-          }
-        })
-
+  console.log("SESSION =====> ", req.session);
+  try {
+    const { username, password } = req.body;
+    const currentUser = await User.findOne({ username });
+    if (!currentUser) {
+      res.render("auth/login", { errorMessage: "No user with this username" });
+    } else {
+      if (bcrypt.compareSync(password, currentUser.password)) {
+        req.session.user = currentUser;
+        res.redirect("/profile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password" });
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.render("auth/signup", { errorMessage: error.message });
+  }
+});
 
 router.get("/logout", (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            next(err)
-        }
-        res.redirect("/")
-    })
-})
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+    }
+    res.redirect("/");
+  });
+});
 
-
-  module.exports = router;
+module.exports = router;
