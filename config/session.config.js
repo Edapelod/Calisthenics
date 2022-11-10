d36c03179ee3da32a121a4f476303e568ba95a33
ItsMrDav <davutt.simsek@gmail.com>
2022-11-10 12:04:09 +0100

    functionality changes

--- config/session.config.js
+++ config/session.config.js
@@ -1,18 +1,18 @@
 // config/session.config.js
 
 // require session
-const session = require('express-session');
+const session = require("express-session");
 
 // since we are going to USE this middleware in the app.js,
 // let's export it and have it receive a parameter
-const MongoStore = require('connect-mongo');
-module.exports = app => {
+const MongoStore = require("connect-mongo");
+module.exports = (app) => {
   // <== app is just a placeholder here
   // but will become a real "app" in the app.js
   // when this file gets imported/required there
 
   // required for the app when deployed to Heroku (in production)
-  app.set('trust proxy', 1);
+  app.set("trust proxy", 1);
 
   // use session
   app.use(
@@ -20,13 +20,20 @@ module.exports = app => {
       secret: process.env.SESS_SECRET,
       resave: true,
       saveUninitialized: false,
-      store: MongoStore.create({ mongoUrl: "mongodb://localhost/Calisthenics" }),
+      store: MongoStore.create({
+        mongoUrl: "mongodb://localhost/Calisthenics",
+      }),
       cookie: {
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 60000 * 60 * 24 // 60 * 1000 ms === 1 min
-      }
+        maxAge: 60000 * 60 * 24, // 60 * 1000 ms === 1 min
+      },
     })
   );
+  // middle ware for making the user available to all templates
+  app.use((req, res, next) => {
+    res.locals.user = req.session.user;
+    next();
+  });
 };
--- routes/profile.js
+++ routes/profile.js
@@ -1,59 +1,95 @@
 const router = require("express").Router();
-const User = require("../models/User.model.js")
-const {isLoggedIn} = require('../middleware/route-guard.js');
-
+const User = require("../models/User.model.js");
+const { isLoggedIn } = require("../middleware/route-guard.js");
 
 router.get("/", isLoggedIn, async (req, res, next) => {
-  let date1 =  await User.findById(req.session.user._id)
-  let date3 = date1.createdAt.toLocaleDateString()
-  console.log(date3, date1)
-  const sum = date1.pushup + date1.squat
-  let level
-  if (sum >= 1 && sum <30) {
-    level = "Beginner ⭐️"
-  } else if (sum >= 30 && sum <40) {
-    level = "Intermediate ⭐️⭐️"
-  } else if (sum >=40 && sum <60) {
-    level = "Advanced ⭐️⭐️⭐️"
+  let date1 = await User.findById(req.session.user._id);
+  let date3 = date1.createdAt.toLocaleDateString();
+  let updateDate = date1.updatedAt.toLocaleDateString();
+  let goodUpdateDate = updateDate;
+
+  const sum = date1.pushup + date1.squat;
+  let level;
+  if (sum >= 1 && sum < 30) {
+    level = "Beginner ⭐️";
+  } else if (sum >= 30 && sum < 40) {
+    level = "Intermediate ⭐️⭐️";
+  } else if (sum >= 40 && sum < 60) {
+    level = "Advanced ⭐️⭐️⭐️";
   } else if (sum > 60) {
-    level = "Expert ⭐️⭐️⭐️⭐️"
+    level = "Expert ⭐️⭐️⭐️⭐️";
   } else {
-    level = "No routine yet"
+    level = "No routine yet";
   }
-  let profileButton 
-if (!date1.pushup) {
-  profileButton = "Create Routine"
-} else {
-  profileButton = "Edit level"
-}
-  res.render("profile", { userConnected: req.session.user, date3, level, profileButton });
+
+  let profileText;
+  if (!date1.pushup) {
+    profileText = "Create Routine?";
+  } else {
+    profileText = "Edit Routine?";
+  }
+
+  let profileButton;
+  if (!date1.pushup) {
+    profileButton = "Create Routine";
+  } else {
+    profileButton = "Edit level";
+  }
+  res.render("profile", {
+    userConnected: req.session.user,
+    date3,
+    level,
+    profileButton,
+    profileText,
+    goodUpdateDate,
+  });
 });
 
 router.post("/", isLoggedIn, async (req, res) => {
-  let date1 =  await User.findById(req.session.user._id)
-  let date3 = date1.createdAt.toLocaleDateString()
-  console.log(date3, date1)
-  const sum = date1.pushup + date1.squat
-  let level 
-  if (sum >= 1 && sum <30) {
-    level = "Beginner ⭐️"
-  } else if (sum >= 30 && sum <40) {
-    level = "Intermediate ⭐️⭐️"
-  } else if (sum >=40 && sum <60) {
-    level = "Advanced ⭐️⭐️⭐️"
+  let date1 = await User.findById(req.session.user._id);
+  let date3 = date1.createdAt.toLocaleDateString();
+  let updateDate = date1.updatedAt.toLocaleDateString();
+  let goodUpdateDate = updateDate;
+
+  const sum = date1.pushup + date1.squat;
+  let level;
+  if (sum >= 1 && sum < 30) {
+    level = "Beginner ⭐️";
+  } else if (sum >= 30 && sum < 40) {
+    level = "Intermediate ⭐️⭐️";
+  } else if (sum >= 40 && sum < 60) {
+    level = "Advanced ⭐️⭐️⭐️";
   } else if (sum > 60) {
-    level = "Expert ⭐️⭐️⭐️⭐️"
+    level = "Expert ⭐️⭐️⭐️⭐️";
   } else {
-    level = "No routine yet"
+    level = "No routine yet";
   }
+
+  let profileText;
   if (!date1.pushup) {
-    profileButton = "Create Routine"
+    profileText = "Create Routine?";
   } else {
-    profileButton = "Edit level"
+    profileText = "Edit Routine?";
   }
-  const updatedUser = await User.findByIdAndUpdate(req.session.user._id, req.body)
-      res.redirect("/profile")
-      res.render("profile", { userConnected: req.session.user, date3, level, profileButton})
-})
+
+  if (!date1.pushup) {
+    profileButton = "Create";
+  } else {
+    profileButton = "Edit";
+  }
+  const updatedUser = await User.findByIdAndUpdate(
+    req.session.user._id,
+    req.body
+  );
+  res.redirect("/profile");
+  res.render("profile", {
+    userConnected: req.session.user,
+    date3,
+    level,
+    profileButton,
+    profileText,
+    goodUpdateDate,
+  });
+});
 
 module.exports = router;
