a0a9e1f14f3d984fc3449a450d24e3ac08b3d877
ItsMrDav <davutt.simsek@gmail.com>
2022-11-11 08:45:21 +0100

    last design added

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Calisthenics JS imported successfully!");
 });
+
+const activeButtonLeft = document.getElementById(`active-button-left`);
+const activeButtonRight = document.getElementById(`active-button-right`);
+
+const infoLeft = document.querySelector(`.hidden-info-left`);
+const infoRight = document.querySelector(`.hidden-info-right`);
+
+activeButtonLeft.addEventListener(`click`, function () {
+  infoLeft.classList.toggle(`hidden`);
+});
+activeButtonRight.addEventListener(`click`, function () {
+  infoRight.classList.toggle(`hidden`);
+});
